70c503e26b447bd8db7e54f8619b6f6f743f5b7e
Vlad <vlad.puzin@gmail.com>
2019-10-24 21:57:03 +0300

    added time limits for console quiz

--- js/public/console-questions.js
+++ js/public/console-questions.js
@@ -1,47 +1,72 @@
-$.getJSON("../../json/question.json", function (json) {
-    function runQuiz(questions) {
-        return answersCount = questions.map(function (item) {
-            var question = item.text;
-            var answer = prompt(question);
+var startDate = new Date();
+var timeForTestInMs = 15000;
+var endDate = startDate.getTime() + timeForTestInMs;
 
-            return questions.answer === answer;
-        });
-    }
+function hasNextQuestion(questions) {
+    return questions.length > 0;
+}
 
-    function countRightAnswers(answers) {
-        return answers.filter(x => x == true).length;
-    }
+function askNextQuestion(text) {
+    return prompt(text);
+}
 
-    function showResultOfQuiz(rightAnswers) {
-        switch (rightAnswers) {
-            case 10:
-            case 9:
-                alert('Ваш IQ > 200. Вы гений!');
-                break;
+function countRightAnswers(answers) {
+    return answers.filter(x => x == true).length;
+}
 
-            case 8:
-            case 7:
-                alert('Ваш Iq около 180. Почти как у Перельмана!');
-                break;
-
-            case 6:
-            case 5:
-            case 4:
-                alert('Ваш Iq около 100. Пойдет, выше среднего.');
-                break;
-            case 3:
-            case 2:
-            case 1:
-            case 0:
-                alert('Ваш Iq 30. Как и у создателя теста!');
-                break;
+function mapToQuestionAnswer(questions) {
+    return questions.map(function(x) {
+        return {
+            "text" : x.text,
+            "answer" : x.answer
         };
+    })
+}
+
+function showResultOfQuiz(rightAnswers) {
+    switch (rightAnswers) {
+        case 10:
+        case 9:
+            alert('Ваш IQ > 200. Вы гений!');
+            break;
+
+        case 8:
+        case 7:
+            alert('Ваш Iq около 180. Почти как у Перельмана!');
+            break;
+
+        case 6:
+        case 5:
+        case 4:
+            alert('Ваш Iq около 100. Пойдет, выше среднего.');
+            break;
+        case 3:
+        case 2:
+        case 1:
+        case 0:
+            alert('Ваш Iq 30. Как и у создателя теста!');
+            break;
+    };
+}
+
+function isTimerEnabled() {
+    var now = new Date();
+    return now <= endDate;
+}
+
+
+$.getJSON("../../json/question.json", function (serilizedJson) {
+    var questions = mapToQuestionAnswer(serilizedJson);
+    alert("Готовы к прохождению новейшего теста на iq? Думайте быстро, у вас 15 секунд!");
+
+    var answers = [];
+    while (hasNextQuestion(questions) && isTimerEnabled()) {
+        var currentAnswer = askNextQuestion(questions[0]["text"]);
+        var rightAnswer = questions[0]["answer"];
+        answers.push(currentAnswer == rightAnswer);
+        questions.splice(0, 1);
     }
 
-    var questions = json;
-    alert("Готовы к прохождению новейшего теста на iq?");
-
-    answers = runQuiz(questions);
-    rightAnswers = countRightAnswers(answers);
-    showResultOfQuiz(rightAnswers);
+    var rightAnswersCount = countRightAnswers(answers);
+    showResultOfQuiz(rightAnswersCount);
 });
