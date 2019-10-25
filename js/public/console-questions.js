9416e6a0de6df71308d179890cf6d87fec6ac946
Vlad <vlad.puzin@gmail.com>
2019-10-25 22:26:09 +0300

    console quiz transfered to es6 format

--- js/public/console-questions.js
+++ js/public/console-questions.js
@@ -1,72 +1,76 @@
-var startDate = new Date();
-var timeForTestInMs = 15000;
-var endDate = startDate.getTime() + timeForTestInMs;
-
-function hasNextQuestion(questions) {
-    return questions.length > 0;
-}
-
-function askNextQuestion(text) {
-    return prompt(text);
-}
-
-function countRightAnswers(answers) {
-    return answers.filter(x => x == true).length;
-}
-
-function mapToQuestionAnswer(questions) {
-    return questions.map(function(x) {
-        return {
-            "text" : x.text,
-            "answer" : x.answer
-        };
-    })
-}
-
-function showResultOfQuiz(rightAnswers) {
-    switch (rightAnswers) {
-        case 10:
-        case 9:
-            alert('Ваш IQ > 200. Вы гений!');
-            break;
-
-        case 8:
-        case 7:
-            alert('Ваш Iq около 180. Почти как у Перельмана!');
-            break;
-
-        case 6:
-        case 5:
-        case 4:
-            alert('Ваш Iq около 100. Пойдет, выше среднего.');
-            break;
-        case 3:
-        case 2:
-        case 1:
-        case 0:
-            alert('Ваш Iq 30. Как и у создателя теста!');
-            break;
-    };
-}
-
-function isTimerEnabled() {
-    var now = new Date();
-    return now <= endDate;
-}
-
-
 $.getJSON("../../json/question.json", function (serilizedJson) {
-    var questions = mapToQuestionAnswer(serilizedJson);
-    alert("Готовы к прохождению новейшего теста на iq? Думайте быстро, у вас 15 секунд!");
+    let startDate = new Date();
+    let timeForTestInMs = 15000;
+    let endDate = startDate.getTime() + timeForTestInMs;
 
-    var answers = [];
+    function isTimerEnabled() {
+        let now = new Date();
+        return now <= endDate;
+    }
+
+    function hasNextQuestion(questions) {
+        return questions.length > 0;
+    }
+
+    function askNextQuestion(text) {
+        return prompt(text);
+    }
+
+    function countRightAnswers(answers) {
+        return answers.filter(x => x == true).length;
+    }
+
+    function mapToQuestionAnswer(questions) {
+        return questions.map((x) => {
+            return {
+                text : x.text,
+                answer: x.answer
+            };
+        })
+    }
+
+    function showResultOfQuiz(rightAnswers, totalAnswers) {
+        let message = 'Вы ответили на ' + rightAnswers + '/' + totalAnswers + ' вопросов.\n';
+        switch (rightAnswers) {
+            case 10:
+            case 9:
+                message += 'Ваш IQ > 200. Вы гений! Зачем вам тогда JS?';
+                alert(message);
+                break;
+
+            case 8:
+            case 7:
+                message += 'Ваш Iq около 180. Почти как у Перельмана!';
+                alert(message);
+                break;
+
+            case 6:
+            case 5:
+            case 4:
+                message += 'Ваш Iq около 100. Пойдет, выше среднего.';
+                alert(message);
+                break;
+            case 3:
+            case 2:
+            case 1:
+            case 0:
+                message += 'Ваш Iq < 30. Как и у создателя теста!';
+                alert(message);
+                break;
+        };
+    }
+
+    let questions = mapToQuestionAnswer(serilizedJson);
+    alert("Готовы к прохождению этого инновационного, точного и сложнейшего теста на iq? Думайте быстро, у вас 15 секунд!");
+
+    let answers = [];
     while (hasNextQuestion(questions) && isTimerEnabled()) {
-        var currentAnswer = askNextQuestion(questions[0]["text"]);
-        var rightAnswer = questions[0]["answer"];
+        let currentAnswer = askNextQuestion(questions[0].text);
+        let rightAnswer = questions[0].answer;
         answers.push(currentAnswer == rightAnswer);
         questions.splice(0, 1);
     }
 
-    var rightAnswersCount = countRightAnswers(answers);
-    showResultOfQuiz(rightAnswersCount);
+    let rightAnswersCount = countRightAnswers(answers);
+    showResultOfQuiz(rightAnswersCount, answers.length);
 });
