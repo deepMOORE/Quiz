d33e467cca9bab12f5de9e5a7fade8de7ef38b83
Vlad <vlad.puzin@gmail.com>
2019-12-01 03:00:04 +0300

    added back button

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -7,32 +7,46 @@ import {QuestionViewer} from "./question-viewer";
 /** @type {HTMLFormElement} */
 let form = document.querySelector('.quiz-form');
 
-export function app() {
-    const answerGenerator = new AnswerGenerator();
-    const answerViewer = new AnswerViewer();
-    const questionGenerator = new QuestionGenerator();
-    const questionViewer = new QuestionViewer();
+const answerGenerator = new AnswerGenerator();
+const answerViewer = new AnswerViewer();
+const questionGenerator = new QuestionGenerator();
+const questionViewer = new QuestionViewer();
+const questionRepository = new QuestionsRepository();
 
-    const questionRepository = new QuestionsRepository();
+export function app() {
     let questions = questionRepository.getQuestionUserModels();
 
     let questionIndex = 0;
     form.addEventListener('submit', function (event) {
         event.preventDefault();
 
-        answerViewer.clear();
-        questionViewer.clearHeader();
+        let currentQuestion = questions[questionIndex];
+        showQuestion(currentQuestion, questionIndex);
+
+        questionIndex++;
+    });
+
+    form.addEventListener('reset', function (event) {
+        event.preventDefault();
+
+        questionIndex--;
+        if (questionIndex < 0) {
+            questionIndex = 0;
+        }
 
         let currentQuestion = questions[questionIndex];
-        let questionPreviewHTML = questionGenerator.generatePreview(questionIndex + 1); // Normal people start counting with zero
-        let questionTextHTML = questionGenerator.generateQuestionText(currentQuestion.text);
-        let answersHTML = answerGenerator.generate(currentQuestion.variants, currentQuestion.type);
-
-        answerViewer.view(answersHTML);
-        questionViewer.viewHeader(questionPreviewHTML, questionTextHTML);
-        questionIndex++;
+        showQuestion(currentQuestion, questionIndex);
     })
 }
 
+function showQuestion(question, index) {
+    answerViewer.clear();
+    questionViewer.clearHeader();
 
+    let questionPreviewHTML = questionGenerator.generatePreview(index + 1); // Normal people start counting with zero
+    let questionTextHTML = questionGenerator.generateQuestionText(question.text);
+    let answersHTML = answerGenerator.generate(question.variants, question.type);
 
+    answerViewer.view(answersHTML);
+    questionViewer.viewHeader(questionPreviewHTML, questionTextHTML);
+}
